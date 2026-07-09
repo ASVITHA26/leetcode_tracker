9d434f97dc585ab32d8d5ac0208ddf8dc76ae357
ASVITHA26 <asvithak26@gmail.com>
2026-07-09 18:08:11 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 18:08:08
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
