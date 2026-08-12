e4bdf1df757f3ec704a1c1883893a0af07f63bd5
ASVITHA26 <asvithak26@gmail.com>
2026-08-12 14:17:37 +0530

    Create 2826-SortingThreeGroups

---
+++ 2826-SortingThreeGroups/version/java/2826-SortingThreeGroups_20260812_141733.java
@@ -0,0 +1,17 @@
+// Last updated: 12/08/2026, 14:17:33
+1class Solution {
+2    public boolean isValid(String s) {
+3        Stack<Character>st = new Stack<>();
+4        for(char ch : s.toCharArray()){
+5            st.push(ch);
+6            if(st.size()>=3){
+7                if(st.peek()=='c' && st.get(st.size()-2)=='b' && st.get(st.size()-3)=='a'){
+8                    st.pop();
+9                    st.pop();
+10                    st.pop();
+11                }
+12            }
+13        }
+14        return st.isEmpty();
+15    }
+16}
