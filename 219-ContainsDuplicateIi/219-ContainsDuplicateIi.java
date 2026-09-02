915ec5c2ab761b6f2f9100af33e2c587740fa2c4
ASVITHA26 <asvithak26@gmail.com>
2026-09-02 09:49:41 +0530

    Create 219-ContainsDuplicateIi

---
+++ 219-ContainsDuplicateIi/219-ContainsDuplicateIi.java
@@ -0,0 +1,12 @@
+// Last updated: 02/09/2026, 09:49:37
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() > k) set.remove(nums[i - k]);
+        }
+        return false;
+    }
+}
