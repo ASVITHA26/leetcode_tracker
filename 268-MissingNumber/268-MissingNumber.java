77dcf5dc0c051ea45c81df676105caacc18f1b8d
ASVITHA26 <asvithak26@gmail.com>
2026-09-02 09:49:26 +0530

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 02/09/2026, 09:49:22
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans = ans ^ i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
